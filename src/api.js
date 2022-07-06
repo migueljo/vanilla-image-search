fd8b60b6d07bfc8461108d61a42acfc1bc254a08
Miguel Martínez <migueljomartinez@gmail.com>
2022-07-05 22:22:50 -0500

    Adding local json server

--- src/api.js
+++ src/api.js
@@ -1,9 +1,12 @@
 import fetchHelper from "./utils/fetchHelper"
 
-const baseUrl = 'https://api.unsplash.com'
+// const baseUrl = 'https://api.unsplash.com'
+const baseUrl = 'http://localhost:3004'
 
 async function getImages(text) {
-  const response = await fetchHelper(`${baseUrl}/search/photos?per_page=20&query=${text}`)
+  // const url = `${baseUrl}/search/photos?per_page=20&query=${text}`
+  const url = `${baseUrl}/images`
+  const response = await fetchHelper(url)
   const images = response.results
   return images
 }
